7986171f0be6b240476db310df4cbfd4e6a5f0ec
hedall89 <55693669+hedall89@users.noreply.github.com>
2019-09-25 21:02:30 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        //Decleration
+        Scanner in = new Scanner(System.in);
+        double price = in.nextDouble();
+        int satisfactoryLvl = in.nextInt();
+        int satisfactory = satisfactoryLvl;
+        double tip1 = 20;
+        double tip2 = 15;
+        double tip3 = 10;
+
+
+
+
+
+
+
+
+        System.out.println("");
+    }
+}
