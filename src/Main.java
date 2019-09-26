62fd06a89e0b31086bcf911dfcdc0c2f7180b507
robert <rhskaar@gmail.com>
2019-09-26 15:41:40 +0200

    completed computation, output of satisfactory lvl and the tip amount TODO

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
@@ -6,27 +7,44 @@ public class Main {
     public static void main(String[] args) {
         //Decleration
         Scanner in = new Scanner(System.in); // scanner created
-        double price; // this should be initialized during coding from input
-        int satisfactory; // this should be initialized during coding from input
-        double tip1 = 20; // tip if satisfactory == 1
-        double tip2 = 15; // tip if satisfactory == 2
-        double tip3 = 10; // tip if satisfactory == 3
+        double price = 0; // this should be initialized during coding from input
+        int satisfactory = 0; // this should be initialized during coding from input
+        double tip1 = 0.20; // tip if satisfactory == 1
+        double tip2 = 0.15; // tip if satisfactory == 2
+        double tip3 = 0.10; // tip if satisfactory == 3
+        double tipAmount = 0;
 
         //input and validation
-        System.out.println("enter price");
-        price = in.nextDouble();
-        satisfactory = 0;
-        System.out.println("Enter satisfactoryLvl like 1, 2, 3");
-        satisfactory = in.nextInt();
+        System.out.print("\n Enter price: ");
+        if (in.hasNextDouble()) {
+            price = in.nextDouble();
+            System.out.print("\n Satisfactory levels: \n    1 = Totally Satisfied,  \n    2 = Satisfied,  \n    3 = Dissatisfied");
+            System.out.print("\n Enter satisfactory level either 1, 2, 3: ");
+            if (in.hasNextInt()) {
+                satisfactory = in.nextInt();
 
-        if (satisfactory == 1)//validation
-            System.out.print("You get 20 percent of the price as tip");
-        else if (satisfactory == 2)
-            System.out.print("you get 15 percent of the price as tip");
-        else if (satisfactory == 3)
-            System.out.print("you get 10 percent of the price as tip");
-        else
-            System.out.println("number not valid, please enter 1, 2 or 3");
+
+                //computation
+                if (satisfactory == 1) {
+                    tipAmount = price * tip1;
+                } else if (satisfactory == 2) {
+                    tipAmount = price * tip2;
+                } else if (satisfactory == 3) {
+                    tipAmount = price * tip3;
+                } else {
+                    System.out.print("\n number not valid, please enter 1, 2 or 3");
+                }
+            }
+            else{
+                System.out.print("\n Satisfactory level should be 1, 2, or 3");
+            }
+        }
+        else {
+            System.out.print("\n the price must be numbers");
+        }
+
+
+        //output TODO - output the satisfaction lvl and the tipAmount
 
     }
 }
