49e413889b7438af6170159a46d11223c9177975
robert <rhskaar@gmail.com>
2019-09-26 12:44:45 +0200

    Updated declaration

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,12 @@ public class Main {
     public static void main(String[] args)
     {
         //Decleration
-        Scanner in = new Scanner(System.in);
-        double price = in.nextDouble();
-        int satisfactoryLvl = in.nextInt();
-        int satisfactory = satisfactoryLvl;
-        double tip1 = 20;
-        double tip2 = 15;
-        double tip3 = 10;
+        Scanner in = new Scanner(System.in); // scanner created
+        double price; // this should be initialized during coding from input
+        int satisfactory; // this should be initialized during coding from input
+        double tip1 = 20; // tip if satisfactory == 1
+        double tip2 = 15; // tip if satisfactory == 2
+        double tip3 = 10; // tip if satisfactory == 3
 
-
-
-
-
-
-
-
-
-        System.out.println("");
     }
 }
